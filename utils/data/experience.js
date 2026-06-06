e1eb95ac39b51d856cc616ced34b66c7a6a09fb3
Mady <madymurugan.2u@gmail.com>
2026-06-06 17:31:58 +0530

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -10,7 +10,7 @@ export const experiences = [
     title: "Mentor",
     company: "G-Tec Computer Education",
     duration: "(March 2025 - June 2025)"
-  }
+  },
   {
    id: 3,
    title: "Customer Care Executive",
